c652622d8f326bdc69bf930d00a8be2618fae17b
pataquets <amontero@tinet.org>
2020-05-07 01:35:59 +0200

    Allow getting default settings from OS environment variables.

--- grip/settings.py
+++ grip/settings.py
@@ -5,26 +5,27 @@ Do NOT change the values here for risk of accidentally committing them.
 Override them using command-line arguments or with a settings_local.py in
 this directory or in ~/.grip/settings.py instead.
 """
+import os
 
 
-HOST = 'localhost'
-PORT = 6419
-DEBUG = False
-DEBUG_GRIP = False
-CACHE_DIRECTORY = 'cache-{version}'
-AUTOREFRESH = True
-QUIET = False
+HOST = os.getenv('GRIP_HOST', 'localhost')
+PORT = os.getenv('GRIP_PORT', 6419)
+DEBUG = os.getenv('GRIP_DEBUG', False)
+DEBUG_GRIP = os.getenv('GRIP_DEBUG_GRIP', False)
+CACHE_DIRECTORY = os.getenv('GRIP_CACHE_DIRECTORY', 'cache-{version}')
+AUTOREFRESH = os.getenv('GRIP_AUTOREFRESH', True)
+QUIET = os.getenv('GRIP_QUIET', False)
 
 
 # Note: For security concerns, please don't save your GitHub password in your
 # local settings.py. Use a personal access token instead:
 # https://github.com/settings/tokens/new?scopes=
-USERNAME = None
-PASSWORD = None
+USERNAME = os.getenv('GRIP_USERNAME')
+PASSWORD = os.getenv('GRIP_PASSWORD')
 
 
 # Custom GitHub API
-API_URL = None
+API_URL = os.getenv('GRIP_API_URL')
 
 
 # Custom styles
